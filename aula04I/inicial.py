0ac816643d4d7474f66d8bf00ec6ae9aae5bfbc1
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-05-27 15:02:56 -0300

    feat: aprendi sobre S single responsability

---
+++ aula04I/inicial.py
@@ -0,0 +1,7 @@
+class SistemaCadastral:
+    def cadastrar(self, nome: str, idade: int) -> None:
+        if isinstance(nome, str) and isinstance(idade, int): #1
+            print("Acessando o banco de dados....") #2
+            print(f"Cadastrar Usuario {nome}, idade {idade}")
+        else:
+            print("Dados Invalidos") #3
