e9f9b52eb9af2291cffa3c21328ff2348684921b
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-06-05 15:16:07 -0300

    feat: learning about private method in Python

--- codecamp/OOP_CodeCamp04.py
+++ codecamp/OOP_CodeCamp04.py
@@ -1,6 +1,9 @@
 # Accessing and Modifying Data:
 # 1. The tarditional way: Make the data private and use Getters and Setters:
 
+from datetime import datetime
+
+# Name Mangled
 class User:
     def __init__(self, username, email, password):
         self.username = username
@@ -8,10 +11,11 @@ class User:
         self.password = password
 
     def get_email(self):
+        print(f"Email accesssed at {datetime.now()}")
         return self._email
 
-    def set_email(self, email) -> None:
-        self._email = email
+    def set_email(self, new_email) -> None:
+        self._email = new_email
         return self._email
 
     def clean_email(self) -> None:
@@ -25,8 +29,9 @@ class User:
 # Test
 user01 = User("danthemna", "Dan@gmail.com ", "123")
 print(user01.get_email())
-print(user01._email) # Estado de protegido é apenas uma convenção em Python. Podemos Acessa-lo fora da classe da mesma forma. 
+print(user01.clean_email())
+print(user01.set_email("Danthemna@gmail.com"))
 print(user01.clean_email())
 
 user02 = User("batman", "bat@outlook.com", "456")
-user01.say_hi_to_user(user02)
+user01.say_hi_to_user(user02) 
